bb772708cd2ac44959a14f28f3ecbef593d1f0fe
waibaBikash <kavrelibikash@gmail.com>
2024-06-10 20:20:37 +1000

    complte title section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
+import Title from './components/Title/Title';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <div className="container">
+        <Title subTitle='Our PROGRAM' title='What We Offer' />
         <Programs />
       </div>
     </div>
--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Title.css';
 
-const Title = () => {
+const Title = ({subTitle, title}) => {
   return (
-    <div>
-
+    <div className='title'>
+      <p>{subTitle}</p>
+      <h2>{title}</h2>
     </div>
   )
 }
