13dbaf653b54236610a2e662ce66a6323c2afd61
waibaBikash <kavrelibikash@gmail.com>
2024-06-10 17:57:05 +1000

    complte programs page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+import Programs from './components/Programs/Programs';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Programs />
+      </div>
     </div>
   )
 }
