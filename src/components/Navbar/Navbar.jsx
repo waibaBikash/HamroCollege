20a1b03c6a0026fc05ed46bf0b6a875adb5e60b1
waibaBikash <kavrelibikash@gmail.com>
2024-06-10 14:15:55 +1000

    added ouufit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Navbar.css';
+import logo from '../../assets/logo.png';
 
 const Navbar = () => {
   return (
     <nav>
-      <img src="" alt="" />
+      <img src={logo} alt="" />
+      <ul>
+         <li>Home</li>
+         <li>Program</li>
+         <li>About us</li>
+         <li>Campus</li>
+         <li>Testimoinals</li>
+         <li>Contact us</li>
+      </ul>
     </nav>
   )
 }
