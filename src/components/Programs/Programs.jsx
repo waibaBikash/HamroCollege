49e0598db412fc043e9efb2d7a2307b9bbbf7a51
waibaBikash <kavrelibikash@gmail.com>
2024-06-10 17:14:51 +1000

    create a porgram page

---
+++ src/components/Programs/Programs.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Programs.css';
+import porgram_1 from '../../assets/porgram_1.png'
+import porgram_2 from '../../assets/porgram_2.png'
+import porgram_3 from '../../assets/porgram_3.png'
+
+const Programs = () => {
+  return (
+    <div programs>
+        <div className="porgram">
+           <img src={porgram_1.png} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Programs;
