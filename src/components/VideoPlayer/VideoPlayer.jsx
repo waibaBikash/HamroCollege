e8d59133d0bfff1d6dcc5e413e6a33996d82d923
waibaBikash <kavrelibikash@gmail.com>
2024-06-11 16:22:17 +1000

    close video player functionality

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './VideoPlayer.css';
 import video from '../../assets/college-video.mp4';
 
 const VideoPlayer = ({playState, setPlayState}) => {
+
+  const player = useRef(null);
+  const colsePlayer = (e)=>{
+    if (e.target === player.current) {
+      setPlayState(false);
+    }
+  }
   return (
-    <div className={`video-player ${playState ? '' : 'hide'}`}>
+    <div className={`video-player ${playState ? '' : 'hide'}`} ref={player} onClick={colsePlayer}>
        <video src={video} autoPlay muted controls></video>
     </div>
   )
