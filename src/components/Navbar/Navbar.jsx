91c5009701b7523b7410ca8e1e161def1ed35b6b
waibaBikash <kavrelibikash@gmail.com>
2024-06-11 12:57:41 +1000

    complte csroll functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
 
@@ -16,12 +17,12 @@ const Navbar = () => {
     <nav className={`container ${sticky? 'dark-nav':''}`}>
       <img src={logo} alt="" className='logo' />
       <ul>
-         <li>Home</li>
-         <li>Program</li>
-         <li>About us</li>
-         <li>Campus</li>
-         <li>Testimoinals</li>
-         <li><button className='btn'>Contact us</button> </li>
+         <li> <Link to='hero' smooth={true} offset={0} duration={500} >Home</Link></li>
+         <li> <Link to='program' smooth={true} offset={0} duration={500}>Program</Link></li>
+         <li> <Link to='about' smooth={true} offset={0} duration={500}>About us</Link></li>
+         <li> <Link to='campus' smooth={true} offset={0} duration={500}>Campus</Link></li>
+         <li> <Link to='testimonials' smooth={true} offset={0} duration={500}>Testimoinals</Link></li>
+         <li><Link className='btn'>Contact us</Link> </li>
       </ul>
     </nav>
   )
