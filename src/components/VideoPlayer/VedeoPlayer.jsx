2e1f581254514c24b70dce069f228bbac907e5df
waibaBikash <kavrelibikash@gmail.com>
2024-06-11 14:55:56 +1000

    added a video player

---
+++ src/components/VideoPlayer/VedeoPlayer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './VedeoPlayer.css';
+
+const VedeoPlayer = () => {
+  return (
+    <div className='video-player'>
+       <video src={}></video>
+    </div>
+  )
+}
+
+export default VedeoPlayer;
