2f4518fcd3e6771fd76d56be53869cf67aaaaf3e
waibaBikash <kavrelibikash@gmail.com>
2024-08-08 22:15:36 +1000

    commit change

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -21,7 +21,10 @@ const About = ({setPlayState}) => {
           to empower students with the knowledge, skills, and experiences needed to
            excel in the dynamic field of education.
            </p>
-         <p>With a focus on innovation, hands-on lerning, and personalized menroship, our programs prepares aspiring educators to make a meaningful inpact in classrooms, schools, and cooouninities.</p>
+         <p>With a focus on innovation, hands-on lerning, and personalized menroship,
+           our programs prepares aspiring educators to make a meaningful inpact in 
+           classrooms, schools, and cooouninities.
+           </p>
          <p>Whether you aspire to become a teacher, administrator, couselor, or educator, or educational leader, our divese rage of programs offers the perfect pathway to achieve your goals and unlock your full potential in shaping the future of education.</p>
        </div>
     </div>
