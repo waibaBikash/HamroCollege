a66f6c67950f29522f0112e172b06e59a05e1376
waibaBikash <kavrelibikash@gmail.com>
2024-08-18 20:27:42 +1000

    commit change

--- src/components/Campus/Campus.jsx
+++ src/components/Campus/Campus.jsx
@@ -15,7 +15,9 @@ const Campus = () => {
           <img src={gallery_3} alt="" />
           <img src={gallery_4} alt="" />
        </div>
-         <button className='btn dark-btn'>See more here <img src={white_arrow} alt="" /></button>
+         <button 
+         className='btn dark-btn'>See more here <img src={white_arrow} alt="" />
+         </button>
     </div>
   )
 }
