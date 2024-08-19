3a8e117c36fca6f106cafb11f0c51b9a8b5c5daa
waibaBikash <kavrelibikash@gmail.com>
2024-08-19 18:57:20 +1000

    changes made

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,7 +25,9 @@ const Navbar = () => {
          <li> 
            <Link to='hero' smooth={true} offset={0} duration={500} >Home</Link>
           </li>
-         <li> <Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+         <li> 
+           <Link to='program' smooth={true} offset={-260} duration={500}>Program</Link>
+          </li>
          <li> <Link to='about' smooth={true} offset={-250} duration={500}>About us</Link></li>
          <li> <Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
          <li> <Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimoinals</Link></li>
