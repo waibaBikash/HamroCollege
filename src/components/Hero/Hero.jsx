a86f1dc3de69f7a3d7b6e985fc0ac30a1352116d
waibaBikash <kavrelibikash@gmail.com>
2024-08-19 18:55:06 +1000

    commit change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,9 @@ const Hero = () => {
   return (
     <div className='hero container'>
       <div className="hero-text">
-         <h1>We Ensure better education for a better world</h1>
+         <h1>
+           We Ensure better education for a better world
+          </h1>
          <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
          <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
       </div>
