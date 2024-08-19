2680126e163d31adf4f37c02f91d87ec435e029a
waibaBikash <kavrelibikash@gmail.com>
2024-08-19 18:54:36 +1000

    save change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,9 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className='footer'>
-       <p>© 2024 HamroCollege. All rights reserved.</p>
+        <p>
+          © 2024 HamroCollege. All rights reserved.
+        </p>
        <ul> 
          <li>Terms of Services</li>
          <li>Privacy Policy</li>
