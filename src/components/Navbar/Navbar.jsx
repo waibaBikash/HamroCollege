95f914df96b74f649be30c0f6da3181b86788c8c
waibaBikash <kavrelibikash@gmail.com>
2024-08-19 18:57:00 +1000

    save change

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,7 +22,9 @@ const Navbar = () => {
     <nav className={`container ${sticky? 'dark-nav':''}`}>
       <img src={logo} alt="" className='logo' />
       <ul className={mobileMenu?'':'hide-mobile-menu'}>
-         <li> <Link to='hero' smooth={true} offset={0} duration={500} >Home</Link></li>
+         <li> 
+           <Link to='hero' smooth={true} offset={0} duration={500} >Home</Link>
+          </li>
          <li> <Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
          <li> <Link to='about' smooth={true} offset={-250} duration={500}>About us</Link></li>
          <li> <Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
