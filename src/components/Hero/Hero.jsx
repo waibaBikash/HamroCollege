59f649093f79bd3d67b78309009909458f664464
waibaBikash <kavrelibikash@gmail.com>
2024-08-19 18:56:02 +1000

    commit change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,11 @@ const Hero = () => {
          <h1>
            We Ensure better education for a better world
           </h1>
-         <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
+           <p>
+             Our cutting-edge curriculum is designed to empower 
+             students with the knowledge, skills, and experiences
+             needed to excel in the dynamic field of education
+           </p>
          <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
       </div>
     </div>
